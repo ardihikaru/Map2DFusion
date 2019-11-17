d2b80f63e0938b2f4e21f6d639d7971a7780bfeb
Ardihikaru <ardihikaru3@gmail.com>
2019-11-17 11:38:54 +0800

    added bump-version.sh and update .gitignore

---
+++ install.sh
@@ -0,0 +1,8 @@
+
+sudo apt-get install -y libopencv-dev
+sudo apt-get install -y build-essential g++ libqt4-core libqt4-dev libqt4-gui qt4-doc qt4-designer libqt4-sql-sqlite
+sudo apt-get install -y libqglviewer-dev libqglviewer2
+sudo apt-get install -y libboost1.54-all-dev
+sudo apt-get install -y libglew-dev libglew1.10
+sudo apt-get install -y freeglut3 freeglut3-dev
+sudo apt-get install -y libdc1394-22 libdc1394-22-dev libdc1394-utils
